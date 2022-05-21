e9f4aba1f2731559ed0cc76ce89ca8546a7fba10
MAC <user@MACs-MacBook-Pro.local>
2022-05-21 17:59:47 +0100

    browser router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/home/home.component';
 import GamePage from './pages/about/about.component';
 
 
@@ -6,6 +8,9 @@ function App() {
 
   return (
     <div className="App">
+       <Routes>
+         <Route  path={'/'} exact={true}  element={<Home />} />
+     </Routes>
       <GamePage />
     </div>
   );
