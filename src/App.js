5ee92f0fc419422c4163f87457345d6db1a883e1
MAC <user@MACs-MacBook-Pro.local>
2022-05-21 17:52:01 +0100

    finishing touches

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import './App.css';
 import GamePage from './pages/about/about.component';
 
-//ADD SOME FANCY DESIGNS WITH CSS
-//AFTER THAT I WILL SWITCH THE CODE FROM REACT JS TO NEXT JS
 
 function App() {
 
